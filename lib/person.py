aa9c7f1953a0ed1fd8a566e6d8f0eb50ef33b049
marilyn <marilyn.oyoo@student.moringaschool.com>
2024-10-13 17:50:21 +0300

    complete lab

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+    def talk(self):
+        print("Hello World!")
+        # prints "Hello World!"
 
-    #Instance method definition
-    pass
+    def walk(self):
+        print("The person is walking.")
+        # prints "The person is walking."
+fido = Person()
+
+fido.talk()
+# prints "Hello World!"
+
+fido.walk()
+# prints "The person is walking."
